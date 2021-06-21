1cbfd0f167dd002fa8b48770c71c3dbe12821878
Samer Odeh <samerodeh220@gmail.com>
2021-06-21 18:58:00 +0300

    lab 12

--- src/Profile.js
+++ src/Profile.js
@@ -1,24 +1,58 @@
 import React, { Component } from 'react'
 import {withAuth0} from "@auth0/auth0-react"
+import BeastBooks from './Component/BeastBooks'
+import axios from 'axios';
 
 export class Profile extends Component {
     constructor(props){
         super(props);
         this.state = {
-            name: this.props.auth0.user.name,
+            username: this.props.auth0.user.name,
             userPicture:this.props.auth0.user.picture,
             userEmail:this.props.auth0.user.email,
+            serverUrl: process.env.REACT_APP_SERVER_URL,
+            booksData: []
         }
     }
+
+    componentDidMount = () => {
+        axios.get(`${this.state.serverUrl}/books?email=${this.state.userEmail}`).then(response => {
+           
+            this.setState({
+                booksData: response.data.books
+            })
+
+            console.log('response',response);
+            
+        }).catch(
+            error => {
+                alert(error.message);
+            }
+        );
+        console.log('axios',this.state.booksData);
+        
+    }
+
     render() {
         return (
             <div>
-                
-                <h2> {this.state.name} </h2>
-                <p> {this.state.userEmail} </p>
-                <img src= {this.state.userPicture} alt={this.state.name} />
 
-            </div>
+              
+                
+                <h2> {this.state.username} </h2>
+                <p> {this.state.userEmail} </p>
+                <img src= {this.state.userPicture} alt={this.state.username} />
+
+               
+            { < BeastBooks 
+                
+                booksData = {this.state.booksData}
+                
+                />
+            }
+            </div> 
+
+            
         )
     }
 }
